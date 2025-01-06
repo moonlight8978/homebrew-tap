beb5f9b1039cbffb360243e62438cd20af92811d
_MoonLight_ <moonlight8978@gmail.com>
2025-01-07 00:01:44 +0700

    feat: add bluestacks air cask

---
+++ Casks/bluestacks-air.rb
@@ -0,0 +1,34 @@
+cask "bluestacks-air" do
+  arch arm: "mac-arm"
+
+  version "5.21.670.7509,6c973c3d266b456bbe61dbfc32c1d5db"
+
+  sha256 arm: "93c20a7efa08db65f6e9e85296532cccc5658b374f5380dd38dbe3e14922705d"
+
+  name "BlueStacks Air"
+  desc "BlueStacks Air Beta for Mac"
+  url "https://ak-build.bluestacks.com/public/app-player/mac/nxt_mac2/#{version.csv.first}/#{version.csv.second}/BlueStacks_#{version.csv.first}.dmg"
+  homepage "https://www.bluestacks.com/mac"
+
+  # https://github.com/Homebrew/homebrew-cask/blob/71e08ccebc2e39e65e69d462d1a36073a1afc708/Casks/b/bluestacks.rb#L10
+  livecheck do
+    url "https://cloud.bluestacks.com/api/getdownloadnow?platform=mac&win_version=&mac_version=#{MacOS.full_version}"
+    regex(%r{/(\d+(?:\.\d+)*)/([^/]+)/}i)
+    strategy :header_match do |headers, regex|
+      match = headers["location"]&.match(regex)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  depends_on macos: ">= :sonoma"
+
+  app "BlueStacks.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.now.gg.BlueStacks.plist",
+    "~/Library/Saved Application State/com.now.gg.BlueStacks.savedState",
+    "~/Library/HTTPStorages/com.now.gg.BlueStacks",
+  ]
+end
