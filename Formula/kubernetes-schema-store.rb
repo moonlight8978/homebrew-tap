3f6c1dd340245856c24c2667937e0ed8130cf2f0
_MoonLight_ <moonlight8978@gmail.com>
2025-09-17 10:29:29 +0700

    feat: add kubernetes-schema-store

---
+++ Formula/kubernetes-schema-store.rb
@@ -0,0 +1,24 @@
+class KubernetesSchemaStore < Formula
+  url "https://github.com/moonlight8978/kubernetes-schema-store/releases/download/v0.1.0/kubernetes-schema-store_darwin_arm64.tar.gz"
+  sha256 "e5d0be40fda10ffcac26a7732fb40e8a7220e62709d884d3a45dc8dbac00e5ed"
+
+  homepage "https://github.com/moonlight8978/kubernetes-schema-store"
+  desc "Kubernetes Schema Store"
+  license "MIT"
+  head "https://github.com/moonlight8978/kubernetes-schema-store.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v(\d+\.\d+\.\d+)$/i)
+  end
+
+  depends_on arch: :arm64
+
+  def install
+    bin.install "kss"
+  end
+
+  test do
+    assert_equal true, shell_output("#{bin}/kss version").include?("kubernetes")
+  end
+end
